f07418d8861aa6512df1c5283f4530d2818cf86d
charlestyndal <catyndal@gmail.com>
2024-08-22 13:16:18 -0600

    Getting started on consumption
    just uploading so I can go to lunch and go home and continue working. Not much is done yet

---
+++ Assets/Scripts/Charles/Ship Consumption.cs
@@ -0,0 +1,53 @@
+rusing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : Item
+{
+    [SerializeField] float speed;
+    [SerializeField] float rotation;
+    [SerializeField] float linearDrag;
+    [SerializeField] ParticleSystem boostParticle;
+    [SerializeField] ParticleSystem collisionParticle;
+
+    public float Speed
+    {
+        get { return speed; }
+    }
+
+    public float Rotation
+    {
+        get { return rotation; }
+    }
+
+    public float LinearDrag
+    {
+        get { return linearDrag; }
+    }
+
+    public ParticleSystem BoostParticle
+    {
+        get { return boostParticle; }
+    }
+
+    public override void TakeDamage(Attack attacker)
+    {
+        if (attacker.CompareTag("Enemy"))
+        {
+            currenthealth -= attacker.Damage;
+            if (currenthealth <= 0)
+            {
+                Destroy(gameObject);
+                GameManager.Instance.GameOver();
+                Debug.Log("Ship destroyed");
+            }
+        }
+
+    }
+
+    
+
+    
+
+
+}
