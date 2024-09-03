0c27d2dce27971b8a8c95cf5114a9f2e2b7c6791
Lulu23323 <85493217+Lulu23323@users.noreply.github.com>
2024-09-03 10:40:19 -0600

    particle follow

---
+++ Assets/Scripts/Lulu/ParticleFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ParticleFollow : MonoBehaviour
+{
+    public Rigidbody2D targetRigidbody;  // 目标物体的Rigidbody2D
+    public ParticleSystem particleSystem; // 分离出来的粒子系统
+
+    private void Update()
+    {
+        // 将粒子系统的transform位置与目标物体的位置同步
+        if (targetRigidbody != null && particleSystem != null)
+        {
+            particleSystem.transform.position = targetRigidbody.transform.position;
+        }
+    }
+}
